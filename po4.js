94302cf8acaf91b37e33e68ddcf473e3c8256d8d
EC2 Default User <ec2-user@ip-172-31-4-20.ec2.internal>
2021-05-05 11:07:27 +0000

    変更点などのコメント

--- po4.js
+++ po4.js
@@ -1,20 +1,18 @@
-function update(a){  /*input tag を更新する関数*/
-    document.querySelector('input').value = a  /*value(値)*  +=は「追加」。 = は「上書き」「更新」の意味になります。
-なの                                                         で、+= '1'と書くと、[ボタン]がクリックされるたびに1の文字が追加され、= ''と書くと、valueがクリアされ、表示内容が空っぽになります。*/
+
+function update(_v){  /*input tag を更新する関数*/
+    document.querySelector('input').value = _v /*value(値)*  +=は「追加」。 = は「上書き」「更新」の意味になります。
+　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　 なので、+= '1'と書くと、[ボタン]がクリックされるたびに1の文字が追加され、= ''と書くと、valueがクリアされ、表示内容が空っぽになります。*/
     }
         
-function yuuki(b){ /*数字ボタンが押されたので数字を後ろに追加する*/
-    document.querySelector('input').value += b /*+=にすることで、数字を入力した時に横に並んで表示される*/
+function append(_v){ /*数字ボタンが押されたので数字を後ろに追加する*/
+    document.querySelector('input').value += _v /*+=にすることで、数字を入力した時に横に並んで表示される*/
     }
-    
+
 function calc(){ /*「＝」ボタンが押されたので計算する*/
       const v = document.querySelector( 'input' ).value /*まずはinputに出てくる値を指定*/
-　　try {
 　　　const f = new Function( 'return ' + v )
       update( f().toString() )
-　　} catch( _error ) {
-　　  update( _error )  
-    }
+
 }
     
     /*
@@ -31,7 +29,7 @@ function calc(){ /*「＝」ボタンが押されたので計算する*/
 　　　}
 　　  ////////////////////////
 　　  
-　　  ・軽く解説すると、const f = new Function( 'return ' + v )この行で関数 f を作っています。
+　　  ・const f = new Function( 'return ' + v )この行で関数 f を作っています。
 　　　　標準ビルトインオブジェクトの Function を使って、'input'タグに書かれている文字列を実行する関数 f を作成しています。
 　　　　一般的に、こんなふうに文字列を関数にして実行してしまうのは、「なにを実行されるかわからない」という危険性を持つので避けるべきコードです。特に、サーバー側で動かすようなプログラムでは、脆弱性に直結するので、絶対に使わないでください。
 　　　　update( f().toString() )この行で作った関数 f を実行して、実行結果を文字列に変えてtoString()、さらに 'input'タグを更新update()しています。
